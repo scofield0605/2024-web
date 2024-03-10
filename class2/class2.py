d61245cb8996951e568730ed7a6a248ce92a7aeb
蔡畇豪 <scofield@Scofields-Air.hitronhub.home>
2024-03-10 18:09:34 +0800

    HW

--- class2/class2.py
+++ class2/class2.py
@@ -22,4 +22,25 @@ new_data = Car ("blue") #Add the next element.
 ptr.next = new_data #Connect the elements.
 new_data.next = head #Point to the head for circular architecture.
 ptr = new_data #The position of the pointer should be the position of the new element.
+
+
+new = Car("black") #Add the new element.
+new.next = head #The new first element points to the original head element.
+#Find the element(ptr) which points to the original head element and turns to point to new element.
+ptr = head
+while ptr.next != head:
+    ptr = ptr. next
+ptr.next = new
+head = new #Set new head element. r
+
+## Its your turn 3.3
+new = Car("pink") #Add the new element.
+#Find the element(ptr) which you want the new node to insert after.
+ptr = head
+while ptr.color != "red":
+    ptr = ptr.next
+#Insert new node into the linked list.
+new.next = ptr.next
+ptr.next = new
+
 traverse(head)
